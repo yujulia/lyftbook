551702c360abaeb4be7a683829e7a0757c192386
Julia Yu <juliayu@gmail.com>
2016-01-30 18:30:30 -0800

    people in looks query

--- index.js
+++ index.js
@@ -54,25 +54,41 @@ app.listen(app.get('port'), function() {
  * ================== API
  */
 
+var returnError = function(re, err){
+	console.error(err);
+	re.send({ error: err });
+};
+
+// ------------------- /api/peopleinlook/:id
+
+app.get('/api/peopleinlook/:id', function(request, response){
+
+	var pid = request.params.id;
+
+	var pilQuery = 'SELECT people.id, nickname';
+		pilQuery += 'FROM looks_person, people ';
+		pilQuery += 'WHERE looks_person.person=people.id ';
+		pilQuery += 'AND looks_person.look=' + pid;
+
+	response.send(peopleinlookQuery);
+});
+
+// ------------------- /api/looks
+
 app.get('/api/looks', function(request, response){
-	var looksData;
+
 	var looksQuery = 'SELECT * FROM looks ORDER BY created DESC';
 
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query(looksQuery, function(err, looks) {
 			done();
 			if (err) {
-				console.error(err);
-
-				looksData = { error: err };
-				response.send(looksData);
+				returnError(response, err);
 			} else {
-				looksData = { looks : looks.rows } ;
-				console.log('returning ', looksData);
-				response.send(looksData);
+				// get people
+
+				response.send({ looks : looks.rows });
 			}
 		});
 	});
-
-
 });
