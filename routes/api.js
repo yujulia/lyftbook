0dfdae44767479ff5aefd2163d8e0786b31845ad
Julia Yu <juliayu@gmail.com>
2016-01-30 22:58:09 -0800

    test

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 
 var pg = require('pg');
 
-exports.getLooks = function(request, response) {
-
+var getLooksData = function(callback){
    var looksQuery = 'SELECT looks.id, looks.image, looks.title, looks.info, people.nickname '
       looksQuery += 'FROM looks, looks_person, people ';
       looksQuery += 'WHERE looks.id = looks_person.look AND looks_person.person = people.id '
@@ -13,7 +12,7 @@ exports.getLooks = function(request, response) {
          done();
          if (err) {
             console.error(err);
-            response.send("Error " + err);
+            // response.send("Error " + err);
          } else {
             var data = [];
             var lookup = {};
@@ -31,10 +30,51 @@ exports.getLooks = function(request, response) {
                }
             });
 
-            return response.send({ looks: data });
-            // response.render('pages/index', { looks: data });
+            callback(data);
          }
+
       });
    });
+}
+
+exports.getLooks = function(request, response) {
+
+   var callback = function(data) {
+      response.send(data);
+   }
+
+   // var looksQuery = 'SELECT looks.id, looks.image, looks.title, looks.info, people.nickname '
+   //    looksQuery += 'FROM looks, looks_person, people ';
+   //    looksQuery += 'WHERE looks.id = looks_person.look AND looks_person.person = people.id '
+   //    looksQuery += 'ORDER BY looks.created DESC';
+
+   // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   //    client.query(looksQuery, function(err, looks) {
+   //       done();
+   //       if (err) {
+   //          console.error(err);
+   //          response.send("Error " + err);
+   //       } else {
+   //          var data = [];
+   //          var lookup = {};
+   //          looks.rows.forEach(function(look) {
+   //             if (lookup[look.id]) {
+   //                lookup[look.id].people.push(look.nickname);
+   //             } else {
+   //                lookup[look.id] = {
+   //                   title: look.title,
+   //                   info: look.info,
+   //                   image: look.image,
+   //                   people: [ look.nickname ]
+   //                }
+   //                data.push(lookup[look.id]);
+   //             }
+   //          });
+
+   //          return response.send({ looks: data });
+   //          // response.render('pages/index', { looks: data });
+   //       }
+   //    });
+   // });
 };
 
